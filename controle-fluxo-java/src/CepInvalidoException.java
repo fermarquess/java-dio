e3f65ed98748e197a2cad33c03300a4abf2280de
Fernanda Marques <fermarquessantos@gmail.com>
2023-09-21 15:08:41 -0300

    add estudos sobre exceções

---
+++ controle-fluxo-java/src/CepInvalidoException.java
@@ -0,0 +1,5 @@
+public class CepInvalidoException extends Exception {
+
+    //tratando exceções customizadas
+    
+}
---
+++ controle-fluxo-java/src/FormatadorCepExemplo.java
@@ -0,0 +1,20 @@
+public class FormatadorCepExemplo {
+
+    public static void main(String[] args) {
+        try {
+            String cepFormatado = formatarCep("2323232");
+            System.out.println(cepFormatado);
+        } catch (CepInvalidoException e) {
+            System.out.println("O CEP não corresponde as regras de negócio");
+        }
+    }
+    static String formatarCep(String cep) throws CepInvalidoException{
+        if(cep.length() != 8)
+          throw new CepInvalidoException();
+        
+          //simulando um cep formatado
+          return "23.232-323";
+    }
+}
+
+//tratando exceções customizadas
